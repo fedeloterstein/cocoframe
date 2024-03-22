bf59901ef0a0c0ffc5cb56d010e8fad57997c7f5
Fedev <federicoloterstein@gmail.com>
2024-03-22 17:44:21 -0300

    refactor

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -4,21 +4,22 @@ import { UserRegistrationForm } from "./UserRegistrationForm";
 import { useAccount, useReadContract } from "wagmi";
 import { ConnectWallet } from "./ConnectWallet";
 import abi from "../abis/ProxyPay.json";
-import { ActiveUser } from "./ActiveUser";
+
+const CONTRACT = process.env.CONTRACT_ADDRESS as `0x` || ""
 
 export const Dashboard = () => {
   const account = useAccount();
 
   const result = useReadContract({
     abi: abi.abi,
-    address: "0x9527e437890f5b4a838b855a58524441be4c7bfe",
+    address: CONTRACT,
     functionName: "getUserData",
     args: [account.address],
   });
 
   const balance = useReadContract({
     abi: abi.abi,
-    address: "0x9527e437890f5b4a838b855a58524441be4c7bfe",
+    address: CONTRACT,
     functionName: "getBalance"
   });
 
